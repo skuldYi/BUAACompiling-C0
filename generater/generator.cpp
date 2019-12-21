7b7db8d39ffce3ac993616b139572410d3f4e4e8
wsy <sywang1207@gmail.com>
2019-12-22 01:02:55 +0800

    basic options in generator

--- generater/generator.cpp
+++ generater/generator.cpp
@@ -1,11 +1,83 @@
 #include "generator.h"
 
 #include <climits>
-#include <sstream>
-#include <ostream>
 
 namespace c0 {
-    struct byteCode* Generate() {
+    byteCode Generator::Generate() {
+        generate();
 
+        byteCode code(_constants, _start, _functions, _instructions);
+
+        return code;
     }
+
+    void Generator::generate() {
+        preTreat();
+
+        int i, len = _quads.size();
+        for (i = 0; i < len && _quads[i].getOperation() != QuadOpr::FUNC; i++) {
+            generateCode(_start, _quads[i]);
+        }
+
+        while (i < len) {
+            int16_t funcId = addFunction(_quads[i++]);
+            _labels.clear();
+            _instructions.emplace_back();
+
+            for (; i < len && _quads[i].getOperation() != QuadOpr::FUNC; i++) {
+                generateCode(_instructions[funcId], _quads[i]);
+            }
+
+            backfillLabel(_instructions[funcId]);
+        }
+    }
+
+
+    void Generator::backfillLabel(std::vector<Instruction>& insSeq) {
+        for (Instruction& i : insSeq) {
+            switch (i.getOpr()) {
+                case opCode::jmp:
+                case opCode::je:	case opCode::jne:
+                case opCode::jl:	case opCode::jge:
+                case opCode::jg:	case opCode::jle:
+                    i.setX(_labels[i.getX()]);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    int Generator::addFunction(const Quadruple& quad) {
+        // FUNC 	name	para_size	level
+        int16_t name = constString(quad.getX().substr(1));
+        int16_t size = std::stoi(quad.getY().substr(1));
+        int16_t level = std::stoi(quad.getR().substr(1));
+
+        _functions.emplace_back(name, size, level);
+        return (int)_functions.size() - 1;
+    }
+
+    int Generator::constString(const std::string & s) {
+        int i;
+        for (i = 0; i < (int)_constants.size(); i++) {
+            if (_constants[i].second == s)
+                return i;
+        }
+        _constants.emplace_back('S', s);
+        return i;
+    }
+
+    int Generator::getFuncId(const std::string & s) {
+        int name = constString(s);
+        for (int i = 0; i < (int)_functions.size(); i++) {
+            if (_functions[i].name_index == name)
+                return i;
+        }
+    }
+
+    void Generator::setLabel(const std::string & label) {
+        _labels[std::stoi(label.substr(1))] = _instructions.size();
+   }
+
 }
--- generater/generator.h
+++ generater/generator.h
@@ -13,20 +13,38 @@
 #include <cstddef> // for std::size_t
 
 namespace c0 {
-    struct byteCode {
-        std::vector<std::pair<char, std::string>> _constants;
-        std::vector<Instruction> _start;
-        std::vector<struct funtionInfo> _functions;
-        std::vector<std::vector<Instruction>> _instructions;
+    class funcInfo {
+    public:
+        std::int16_t name_index;
+        std::int16_t params_size;
+        std::int16_t level;
+
+        funcInfo(int16_t name, int16_t size, int16_t level)
+            : name_index(name), params_size(size), level(level) {}
     };
 
-    struct functionInfo {
-        int name_index;
-        int params_size;
-        int level;
+    class byteCode {
+    public:
+        byteCode(std::vector<std::pair<char, std::string>> constants,
+                 std::vector<Instruction> start,
+                 std::vector<funcInfo> functions,
+                 std::vector<std::vector<Instruction>> instructions)
+                  : constants(std::move(constants)), start(std::move(start)),
+                    functions(std::move(functions)), instructions(std::move(instructions)) {}
+
+        std::vector<std::pair<char, std::string>> constants;
+        std::vector<Instruction> start;
+        std::vector<funcInfo> functions;
+        std::vector<std::vector<Instruction>> instructions;
     };
 
 	class Generator final {
+    private:
+        using uint32_t = std::uint32_t;
+        using int32_t = std::int32_t;
+        using uint16_t = std::uint16_t;
+        using int16_t = std::int16_t;
+
 	public:
 		Generator(std::vector<Quadruple> q)
 		    : _quads(std::move(q)), _constants({}), _start({}), _functions({}), _instructions({}) {}
@@ -35,15 +53,29 @@ namespace c0 {
 		Generator& operator=(Generator) = delete;
 
 		// 唯一接口
-        struct byteCode* Generate();
+        byteCode Generate();
 
     private:
         std::vector<Quadruple> _quads;
+        std::map<int, int> _labels;
 
 	    std::vector<std::pair<char, std::string>> _constants;
 	    std::vector<Instruction> _start;
-	    std::vector<struct funtionInfo> _functions;
+	    std::vector<funcInfo> _functions;
 	    std::vector<std::vector<Instruction>> _instructions;
 
+        void generate();
+
+	    int addFunction(const Quadruple&);
+	    int getFuncId(const std::string&);
+	    int constString(const std::string&);
+
+//	    void preTreat();
+//	    void generateCode(std::vector<Instruction>&, const Quadruple&);
+//	    void getAddr(std::vector<Instruction>&, const std::string&);
+//	    void loadI(std::vector<Instruction>&, const std::string&);
+
+	    void setLabel(const std::string&);
+	    void backfillLabel(std::vector<Instruction>&);
 	};
 }
